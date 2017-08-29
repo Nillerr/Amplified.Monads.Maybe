6113ce9dbf6ecca5fb5b967d997e069bfde2f81e
Nillerr <800751+Nillerr@users.noreply.github.com>
2017-08-29 17:28:11 +0200

    Adds a JsonConverter

---
+++ Amplified.Monads.Maybe.Json/MaybeJsonConverter.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Concurrent;
+using Amplified.Monads.ComponentModel;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Amplified.Monads.Json
+{
+    public sealed class MaybeJsonConverter<T> : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var maybeValue = (Maybe<T>) value;
+            maybeValue.Match(some => serializer.Serialize(writer, some), none => writer.WriteNull());
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var token = serializer.Deserialize<JToken>(reader);
+            if (token.Type == JTokenType.Null)
+                return Maybe<T>.None();
+
+            var value = token.ToObject<T>(serializer);
+            return Maybe<T>.Some(value);
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(Maybe<T>);
+        }
+    }
+
+    public sealed class MaybeJsonConverter : JsonConverter
+    {
+        private static readonly ConcurrentDictionary<Type, JsonConverter> Converters =
+            new ConcurrentDictionary<Type, JsonConverter>();
+
+        private static JsonConverter GetConverter(Type type)
+        {
+            return Converters.GetOrAdd(type, CreateConverter);
+        }
+
+        private static JsonConverter CreateConverter(Type type)
+        {
+            var valueType = type.GenericTypeArguments[0];
+            var converterType = typeof(MaybeJsonConverter<>).MakeGenericType(valueType);
+            var converter = (JsonConverter) Activator.CreateInstance(converterType);
+            return converter;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var actualConverter = GetConverter(value.GetType());
+            actualConverter.WriteJson(writer, value, serializer);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var actualConverter = GetConverter(objectType);
+            return actualConverter.ReadJson(reader, objectType, existingValue, serializer);
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType.IsConstructedGenericType && objectType.GetGenericTypeDefinition() == typeof(Maybe<>);
+        }
+    }
+}
---
+++ Amplified.Monads.Maybe.Tests/Json/JsonConverterTests.cs
@@ -0,0 +1,90 @@
+﻿using Newtonsoft.Json;
+using Xunit;
+
+namespace Amplified.Monads.Json
+{
+    public class JsonConverterTests
+    {
+        private readonly JsonSerializerSettings _settings = new JsonSerializerSettings
+        {
+            Converters = { new MaybeJsonConverter() }
+        };
+        
+        [Fact]
+        public void SerializesSomeInt()
+        {
+            var source = new { Value = Maybe.Some(123) };
+            var result = JsonConvert.SerializeObject(source, _settings);
+            Assert.Equal("{\"Value\":123}", result);
+        }
+        
+        [Fact]
+        public void SerializeSomeString()
+        {
+            var source = new { Value = Maybe.Some("Hello") };
+            var result = JsonConvert.SerializeObject(source, _settings);
+            Assert.Equal("{\"Value\":\"Hello\"}", result);
+        }
+        
+        [Fact]
+        public void SerializeSomeObject()
+        {
+            var source = new { Value = Maybe.Some(new { Foo = "bar" }) };
+            var result = JsonConvert.SerializeObject(source, _settings);
+            Assert.Equal("{\"Value\":{\"Foo\":\"bar\"}}", result);
+        }
+
+        [Fact]
+        public void SerializeNone()
+        {
+            var source = new { Value = Maybe<int>.None() };
+            var result = JsonConvert.SerializeObject(source, _settings);
+            Assert.Equal("{\"Value\":null}", result);
+        }
+
+        [Fact]
+        public void DeserializeSomeInt()
+        {
+            var source = "{\"Value\":834534}";
+            var result = JsonConvert.DeserializeObject<ValueContainer<Maybe<int>>>(source, _settings);
+            Assert.Equal(result.Value, Maybe.Some(834534));
+        }
+
+        [Fact]
+        public void DeserializeSomeString()
+        {
+            var source = "{\"Value\":\"World\"}";
+            var result = JsonConvert.DeserializeObject<ValueContainer<Maybe<string>>>(source, _settings);
+            Assert.Equal(result.Value, Maybe.Some("World"));
+        }
+
+        [Fact]
+        public void DeserializeSomeObject()
+        {
+            var source = "{\"Value\":{\"Value\":\"Mr. Worldwide\"}}";
+            var result = JsonConvert.DeserializeObject<ValueContainer<ValueContainer<Maybe<string>>>>(source, _settings);
+            Assert.Equal(result.Value.Value, Maybe.Some("Mr. Worldwide"));
+        }
+
+        [Fact]
+        public void DeserializeNoneToPrimitive()
+        {
+            var source = "{\"Value\":null}";
+            var result = JsonConvert.DeserializeObject<ValueContainer<Maybe<int>>>(source, _settings);
+            Assert.Equal(result.Value, Maybe<int>.None());
+        }
+
+        [Fact]
+        public void DeserializeNoneToObject()
+        {
+            var source = "{\"Value\":null}";
+            var result = JsonConvert.DeserializeObject<ValueContainer<Maybe<object>>>(source, _settings);
+            Assert.Equal(result.Value, Maybe<object>.None());
+        }
+
+        private sealed class ValueContainer<T>
+        {
+            public T Value { get; set; }
+        }
+    }
+}
